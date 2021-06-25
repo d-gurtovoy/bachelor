466287b825956b3993d594ea024b249a444c630b
d-gurtovoy <57064028+d-gurtovoy@users.noreply.github.com>
2021-06-25 07:11:52 +0200

    Add files via upload

--- naive.py
+++ naive.py
@@ -2,47 +2,52 @@ import json
 import pickledb
 
 #person_label = pickledb.load('personDB.db', False)
-#person_index = pickledb.load('person_index.db',False)
 person_index = pickledb.load('person_index.db', False)
 
 gt = json.load(open('wikidata_all.json','r'))
 suffix = json.load(open('suffixe.json','r'))
 
-res = []
 correct = []
 incorrect = []
 none = []
 total = 4799
 
 for i in gt['results']['bindings']:
-    street_label = i['streetLabel']['value']
-    street_label = street_label.replace('-',' ')
+    item = {
+    'street_label' : (i['streetLabel']['value']).replace('-',' '),
+    'street_id' : (i['street']['value']).replace('http://www.wikidata.org/entity/',''),
+    'street_gt' : (i['person']['value']).replace('http://www.wikidata.org/entity/','')
+    }
 
-    street_id = i['street']['value']
-    street_id = street_id.replace('http://www.wikidata.org/entity/','')
 
-    street_gt = i['person']['value']
-    street_gt = street_gt.replace('http://www.wikidata.org/entity/','')
-
-    for j in suffix:
-        try:
-            street_label = street_label.replace(j,'')
-        except:
-            pass
-    try:
-        result = person_index.get(street_label)
-        if result[0][0] == street_gt:
-            correct.append(street_label)
+    for s in suffix:
+        if s in item['street_label']:
+            query = item['street_label'].replace(s,'')
+            break
         else:
-            incorrect.append(street_label)
+            query = item['street_label']
+    item['query'] = query
+
+    try:
+        result = person_index.get(query)
+        if result[0][0] == item['street_gt']:
+            correct.append(item)
+        else:
+            incorrect.append(item)
     except:
-        none.append(street_label)
-'''
-test = json.dumps(res, indent=4)
+        none.append(item)
 
-with open('test.json','w') as output:
-    output.write(test)
+correct_list = json.dumps(correct, indent=4)
+incorrect_list = json.dumps(incorrect, indent=4)
+none_list = json.dumps(none, indent=4)
 
-'''
+with open('naive_correct.json','w') as output_correct:
+    output_correct.write(correct_list)
+
+with open('naive_incorrect.json','w') as output_cinorrect:
+    output_cinorrect.write(incorrect_list)
+
+with open('naive_none.json','w') as output_none:
+    output_none.write(none_list)
 
 print(len(correct),len(incorrect),len(none))
